eaa6d123ca21249c9ef170b5301142116c2efaf5
K-SEUNGRI <sr930315@naver.com>
2023-01-19 13:19:53 +0900

    20230119

---
+++ main2.py
@@ -0,0 +1,136 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+from openpyxl import Workbook
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+
+print(os.getcwd())
+
+# chromedriver.exe 위치
+path = "C:/Users/sr/git-winner/crawling/chromedriver.exe"
+url = 'https://map.naver.com/v5/search/'
+chrome_options = Options()
+chrome_options.add_argument('--start-maximized')
+# chrome_options.add_argument('--start-fullscreen')
+
+# #브라우저 창 숨기기
+# chrome_options.add_argument('headless')
+# chrome_options.add_argument('--disable-gpu')
+# chrome_options.add_argument('lang=ko_KR')
+
+# #브라우저 실행 및 탭 추가
+driver = webdriver.Chrome( executable_path=path, chrome_options=chrome_options )
+
+#driver = webdriver.Chrome(executable_path=path, chrome_options=options)
+
+# 크롤링 내용 파일로 저장
+wb = Workbook()
+ws = wb.active
+ws.append(['Name','Address','Number'])
+now = time.localtime()
+s = '%04d%02d%02d_%02d%02d' % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min)
+f_name = 'naver_map_crawling_' + s + '.xlsx'
+
+
+driver.get(url + '서교동')
+time.sleep(2)
+
+#음식점 클릭
+if driver.find_element(By.CLASS_NAME,'bubble_filter_list').find_element(By.TAG_NAME,'li').text == '음식점':
+    driver.find_element(By.CLASS_NAME,'bubble_filter_list').find_element(By.TAG_NAME,'li').click()
+    time.sleep(2)
+else:
+    print("첫번째 리스트 '음식점'이 아님")
+
+
+
+
+n = 0
+while n <= 6:
+    # searchIframe 전체적으로 내려서 숨겨진 tag 열기
+    driver.switch_to.default_content()
+    driver.switch_to.frame('searchIframe')
+    time.sleep(1)
+    driver.find_element(By.TAG_NAME,'body').click()
+    time.sleep(1)
+
+    # 스크롤 내리기
+    for i in range(30):
+        driver.find_element(By.TAG_NAME,'body').send_keys(Keys.PAGE_DOWN)
+        time.sleep(0.5)
+
+    driver.switch_to.default_content()
+    driver.switch_to.frame('searchIframe')
+
+    for i in driver.find_element(By.ID,'_pcmap_list_scroll_container').find_elements(By.TAG_NAME,'li'):
+        driver.switch_to.default_content()
+        driver.switch_to.frame('searchIframe')
+        i.click()
+        driver.switch_to.default_content()
+        time.sleep(0.5)
+        driver.switch_to.frame('entryIframe')
+        time.sleep(0.5)
+
+        # flicking-camera 가 넘어가는 경우
+        try:
+            driver.find_element(By.XPATH,'//*[@id="app-root"]/div/div/div/div[5]/div/div/div/a[1]/span').click()
+        except:
+            pass
+
+        driver.find_element(By.CLASS_NAME,'flicking-camera').find_element(By.TAG_NAME,'a').click()
+        time.sleep(3)
+
+        if len(driver.find_element(By.ID,'_title').find_elements(By.TAG_NAME,'span')) == 2:
+            title = driver.find_element(By.ID,'_title').find_element(By.TAG_NAME,'span').text
+            category = driver.find_element(By.ID,'_title').find_elements(By.TAG_NAME,'span')[1].text
+        else:
+            title = driver.find_element(By.ID,'_title').find_elements(By.TAG_NAME,'span')[1].text
+            category = driver.find_element(By.ID,'_title').find_elements(By.TAG_NAME,'span')[2].text
+
+        if '이벤트' in driver.find_elements(By.CLASS_NAME,'place_section_content')[0].text:
+
+            place_section = driver.find_elements(By.CLASS_NAME,'place_section_content')[1]
+        else:
+            place_section = driver.find_elements(By.CLASS_NAME,'place_section_content')[0]
+
+        for i in place_section.find_elements(By.TAG_NAME,'div'):
+            if '주소' in i.text:
+                store_address = i.text.split('\n')[1]
+                break
+
+        for i in place_section.find_elements(By.TAG_NAME,'div')[2:]:
+            if '복사' in i.text:
+                store_number = i.text
+                if '안내' in store_number:
+                    store_number = store_number.split('안내')[0]
+                if '복사' in store_number:
+                    store_number = i.text.split('복사')[0]
+                store_number = store_number.replace('\n', '')
+                break
+            else:
+                store_number = ''
+
+        store = [title, category, store_address, store_number]
+        print(store)
+        ws.append(store)
+        time.sleep(1)
+
+        # 변수 리셋
+        title = ''
+        category = ''
+        store_address = ''
+        store_number = ''
+        wb.save(f_name)
+
+    # 다음페이지가 포함된 태그 위치 클릭
+    driver.switch_to.default_content()
+    driver.switch_to.frame('searchIframe')
+    driver.find_element(By.XPATH,'//span[text()="다음페이지"]/..').click()
+    time.sleep(3)
+    n += 1
+
+wb.save(f_name)
